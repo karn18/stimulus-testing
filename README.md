18f120d639a4427ecd5a4a7d64e2e30f3af3bc52
karn <karn@hey.com>
2021-12-10 09:55:10 +0700

    Add hello_controller

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Awesome Stimulus
 
 [![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com)
+
+With testing, we can sparkle our website by using *STIMULUS* with sparking joy and confidence.
---
+++ test/hello_controller.test.js
@@ -0,0 +1,34 @@
+import { Application } from '@hotwired/stimulus';
+import HelloController from '../src/hello_controller';
+
+describe("HelloController", () => {
+  describe("Greeting", () => {
+    beforeEach(() => {
+      document.body.innerHTML = `<div data-controller="hello">
+        <input data-hello-target="name" type="text">
+        <button data-action="click->hello#greet">
+          Greet
+        </button>
+        <span data-hello-target="output">
+        </span>
+      </div>`;
+
+      const application = Application.start();
+      application.register("hello", HelloController);
+    });
+
+    it("Input name and see the output after click the button", () => {
+      // Arrange
+      const input = document.querySelector("[data-hello-target='name']");
+      const button = document.querySelector("[data-action='click->hello#greet']");
+      const output = document.querySelector("[data-hello-target='output']");
+
+      // Act
+      input.value = "Karn";
+      button.click();
+
+      // Assert
+      expect(output.textContent).toEqual("Hello, Karn!");
+    });
+  });
+});
