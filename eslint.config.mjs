bc299c089fe0245d699d063b5836c80b88e54826
Karn Tirasoontorn <karn@hey.com>
2024-11-10 11:42:49 +0700

    fix: gh action

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-];
+  {
+    languageOptions: { globals: globals.browser },
+    ignores: ['**/*.config.js']
+  },
+  pluginJs.configs.recommended
+]
